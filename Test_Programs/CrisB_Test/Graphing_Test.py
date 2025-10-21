eddbb79484f84f119e84d73eb3961f38d5b237ef
cbres <cbresciani129@yahoo.com>
2025-10-21 17:17:30 -0400

    Edited my graphing test file to change color and dimension

--- Test_Programs/CrisB_Test/Graphing_Test.py
+++ Test_Programs/CrisB_Test/Graphing_Test.py
@@ -12,10 +12,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self.graphWidget)
 
         hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+        temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45,]
         # plot data: x, y values
+        
+        
+        self.setFixedSize(1280,720)
+        #Dimensions of 1280x720
+        self.graphWidget.setBackground("black")
+        
  
-        self.graphWidget.plot(hour, temperature)
+        self.graphWidget.plot(hour, temperature, pen = "r")
+        #pen = r sets red color
+
+
 app = QtWidgets.QApplication(sys.argv)
 main = MainWindow()
 main.show()
